2df7389c125f6b1aaea790f37b8872340b81379c
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2025-10-24 14:11:29 +0200

    init

---
+++ src/WgetDriver.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace Core;
+
+class WgetDriver
+{
+    /** @var int */
+    private static $instancesCount = 0;
+    /** @var string[] */
+    private static $allowed_methods = ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'];
+    /** @var array */
+    private $send_headers = [];
+    /** @var bool */
+    private $flagAsJson = false;
+
+    /** @var */
+    private $curl;
+
+    /**
+     * Constructor
+     */
+    private function __construct()
+    {
+        self::$instancesCount++;
+    }
+
+    /**
+     * Destructor
+     */
+    public function __destruct()
+    {
+        self::$instancesCount--;
+    }
+
+    /**
+     * Create new instance
+     * @return WgetDriver
+     */
+    public static function init()
+    {
+        $instance = new self();
+        $instance->curl = curl_init();
+        //curl_reset($instance->curl);
+        curl_setopt($instance->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($instance->curl, CURLINFO_HEADER_OUT, true);
+        curl_setopt($instance->curl, CURLOPT_VERBOSE, true);
+        curl_setopt($instance->curl, CURLOPT_HEADER, true);
+        curl_setopt($instance->curl,CURLOPT_ENCODING , "gzip");  // TODO: discover this
+        if (config('IGNORE_SSL_ERRORS', false)) {
+            curl_setopt($instance->curl, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($instance->curl, CURLOPT_SSL_VERIFYPEER, 0);
+        }
+        //curl_setopt($instance->curl, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($instance->curl, CURLOPT_FRESH_CONNECT, true);
+        (stripos(PHP_OS, 'win') !== false) && curl_setopt($instance->curl, CURLOPT_SSL_VERIFYPEER, false); // fucking windows for repair https://www.saotn.org/dont-turn-off-curlopt_ssl_verifypeer-fix-php-configuration/
+        return $instance;
+    }
+
+    /**
+     * @param string $bearerHash
+     * @param array $additional_headers
+     * @return $this
+     */
+    public function setBearerAutorisation($bearerHash, array $additional_headers=[])
+    {
+        $this->setHeaders(["Authorization: Bearer $bearerHash"]);
+        $this->setHeaders($additional_headers);
+        return $this;
+    }
+
+    /**
+     * Set headers that request is json
+     * @return $this
+     */
+    public function asJson()
+    {
+        $this->flagAsJson = true;
+        $this->setHeaders(['Content-Type: application/json']);
+        return $this;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    private function setUrl($url)
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        return $this;
+    }
+
+    /**
+     * @param string $method
+     * @return $this
+     */
+    private function setMethod($method)
+    {
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, $method);
+        return $this;
+    }
+
+    /**
+     * @param array|string|null $data
+     * @return $this
+     */
+    private function setData($data)
+    {
+        if (empty($data)) {
+            $data = [];
+        }
+        if ($this->flagAsJson) {
+            $data = json_encode($data);
+        }
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+        return $this;
+    }
+
+    /**
+     * @param array $headers
+     * @return $this
+     */
+    public function setHeaders(array $headers)
+    {
+        $prepared_headers = [];
+        foreach ($headers as $k => $v) {
+            if (gettype($k) === 'string') {
+                $prepared_headers[] = "{$k}: {$v}";
+            } else {
+                $prepared_headers[] = $v;
+            }
+        }
+        $this->send_headers = array_merge($this->send_headers, $prepared_headers);
+        return $this;
+    }
+
+    /**
+     * @param int $port
+     * @return $this
+     */
+    public function setPort($port)
+    {
+        curl_setopt($this->curl, CURLOPT_PORT, $port);
+        return $this;
+    }
+
+    /**
+     * Execute curl and process response
+     * @return WgetResponse
+     */
+    private function exec()
+    {
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->send_headers);
+        return new WgetResponse($this->curl);
+    }
+
+    /**
+     * @param string $url
+     * @param null|string|array $get_data
+     * @param null|string|array $body_data
+     * @return WgetResponse
+     */
+    public function get($url, $get_data = null, $body_data = null)
+    {
+        if (!is_null($get_data)) {
+            $url = trim(trim($url, '?'), '&');
+            if (is_string($get_data)) {
+                $str = trim(trim($get_data, '?'), '&');
+            } elseif (is_array($get_data)) {
+                $str = "";
+                foreach ($get_data as $k => $v) {
+                    if (!is_array($v) && !is_object($v)) {
+                        $str .= $k . '=' . urlencode($v) . '&';
+                    } elseif (is_array($v)) {
+                        foreach ($v as $k2 => $v2) {
+                            $str .= $k . '=' . urlencode($v2) . '&';
+                        }
+                    }
+                }
+            }
+            /**/
+            if (strrpos($url, '?') === false) {
+                $url .= '?' . $str;
+            } else {
+                $url .= '&' . $str;
+            }
+        }
+
+        return $this
+            ->setUrl($url)
+            ->setMethod('GET')
+            ->setData($body_data)
+            ->exec();
+    }
+
+    /**
+     * @param string $url
+     * @param array|string|null $data
+     * @return WgetResponse
+     */
+    public function post($url, $data = null)
+    {
+        return $this
+            ->setUrl($url)
+            ->setMethod('POST')
+            ->setData($data)
+            ->exec();
+    }
+
+    /**
+     * @param string $url
+     * @param array|string|null $data
+     * @return WgetResponse
+     */
+    public function put($url, $data = null)
+    {
+        return $this
+            ->setUrl($url)
+            ->setMethod('PUT')
+            ->setData($data)
+            ->exec();
+    }
+
+    /**
+     * @param string $url
+     * @param array|string|null $data
+     * @return WgetResponse
+     */
+    public function patch($url, $data = null)
+    {
+        return $this
+            ->setUrl($url)
+            ->setMethod('PATCH')
+            ->setData($data)
+            ->exec();
+    }
+
+    /**
+     * @param string $url
+     * @param array|string|null $data
+     * @return WgetResponse
+     */
+    public function delete($url, $data = null)
+    {
+        return $this
+            ->setUrl($url)
+            ->setMethod('DELETE')
+            ->setData($data)
+            ->exec();
+    }
+}
---
+++ src/WgetResponse.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Core;
+
+use Exception;
+
+/**
+ * Class WgetResponse used by WgetDriver
+ * for convenient manage response
+ */
+class WgetResponse
+{
+    /** @var string|false */
+    private $body = false;
+    /** @var string|false */
+    private $request_headers = false;
+    /** @var string|false */
+    private $response_headers = false;
+    /** @var int|false */
+    private $status = false;
+    /** @var string */
+    private $url;
+    /** @var array */
+    private $errors;
+
+    /**
+     * Constructor
+     * execute curl and prepare response
+     * @param $curl_resource
+     */
+    public function __construct($curl_resource)
+    {
+        try {
+            $response = curl_exec($curl_resource);
+            $this->request_headers = curl_getinfo($curl_resource, CURLINFO_HEADER_OUT);
+            if ($response === false) {
+                $this->errors[] = 'Curl return false';
+                $this->errors[] = curl_error($curl_resource);
+                $this->errors[] = curl_errno($curl_resource);
+                return;
+            }
+            $this->url = curl_getinfo($curl_resource, CURLINFO_EFFECTIVE_URL);
+            $this->status = intval(curl_getinfo($curl_resource, CURLINFO_HTTP_CODE));
+            $header_size = curl_getinfo($curl_resource, CURLINFO_HEADER_SIZE);
+            $this->response_headers = mb_substr($response, 0, $header_size);
+            $this->body = mb_substr($response, $header_size);
+        } catch (Exception $e) {
+            $this->errors[] = $e->getMessage();
+        }
+    }
+
+    /**
+     * Return error-stack
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @return string
+     */
+    public function url()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Return body of response
+     * @return false|string
+     */
+    public function body()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Save response as file
+     * @param $file_name
+     * @return bool
+     */
+    public function save($file_name)
+    {
+        $dir = dirname($file_name);
+        if (file_exists($dir) && is_dir($dir) && is_writable($dir)) {
+            file_put_contents($file_name, $this->body());
+            return true;
+        } else {
+            $this->errors[] = "Can't write into this path";
+            return false;
+        }
+    }
+
+    /**
+     * Return response as json
+     * @param null $key
+     * @param mixed $default
+     * @return false|mixed
+     */
+    public function json($key = null, $default = null)
+    {
+        if (gettype($this->body) === 'string') {
+            $decoded = json_decode((string) $this->body, true);
+
+            if (is_null($key)) {
+                return $decoded;
+            }
+
+            return isset($decoded[$key])
+                ? $decoded[$key]
+                : $default;
+        }
+
+        return $default;
+    }
+
+    /**
+     * Return all request-headers of response
+     * @return false|string[]
+     */
+    public function request_headers()
+    {
+        return explode("\n", $this->request_headers);
+    }
+
+    /**
+     * Return all response-headers
+     * @return false|string[]
+     */
+    public function response_headers()
+    {
+        return explode("\n", $this->response_headers);
+    }
+
+    /**
+     * Return status code of response
+     * @return false|int
+     */
+    public function status()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Return all cookies of response
+     * @return array
+     */
+    public function cookies()
+    {
+        /** TODO */
+        return [];
+    }
+}
